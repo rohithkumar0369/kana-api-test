c6a132a3d2b0a027f5db63bd4783ee0204decef0
Haripriya <haripriya14022003@gmail.com>
2025-07-21 18:38:49 +0530

    fix:added etaswap scripts

--- src/samechain/evm.ts
+++ src/samechain/evm.ts
@@ -1,27 +1,28 @@
 import axios from "axios";
-import "dotenv/config";
-import { local, NetworkId } from "../constant";
 
-export const fetchingQuotes = async () => {
-  const response = await axios.get(local.sameChainQuote, {
-    params: {
-      inputToken: "0x0000000000000000000000000000000000000000",
-      outputToken: "0x0000000000000000000000000000000000010348",
-      chain: "295",
-      amountIn: "100000000",
-      slippage: 0,
-      evmExchange: JSON.stringify(["etaSwap"]),
-    },
-    headers: {
-      "Content-Type": "application/json",
-      "X-API-KEY": "//* YOUR API KEY *//", // Your Kana API key
-    },
-  });
-  console.log("🚀 ~ kanaswap ~ response:", response?.data?.data);
-  console.log(
-    "🚀 ~ kanaswap ~ response:",
-    response?.data?.data[0].fromChainId[0]
-  );
+const local = {
+  sameChainQuote: "http://localhost:3000/v1/swapQuote",
 };
 
-fetchingQuotes();
+async function fetchQuote() {
+  try {
+    const response = await axios.get(local.sameChainQuote, {
+      params: {
+        inputToken: "0x0000000000000000000000000000000000000000",
+        outputToken: "0x000000000000000000000000000000000006f89a",
+        chain: "12",
+        amountIn: "100000000",
+        slippage: 0,
+        evmExchange: JSON.stringify(["etaSwap"]),
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("🚀 ~ kanaswap ~ response:", response.data);
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+  }
+}
+
+fetchQuote();
